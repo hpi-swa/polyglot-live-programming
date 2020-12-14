759ed7e3bf83c08846ffb01fc71702200fe4e2e6
TheoRadig <theo.r@hotmail.de>
2020-12-14 14:37:54 +0100

    remove header component

--- src/ng-webview/src/app/app.module.ts
+++ src/ng-webview/src/app/app.module.ts
@@ -4,12 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { BabylonianAnalysisComponent } from './babylonian-analysis/babylonian-analysis.component';
-import { HeaderComponent } from './header/header.component';
 import { MatSliderModule } from '@angular/material/slider';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
 
 @NgModule({
-  declarations: [AppComponent, BabylonianAnalysisComponent, HeaderComponent],
+  declarations: [AppComponent, BabylonianAnalysisComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot([], { useHash: true }),
